0b9a07177294fe43aa5045f5de6623e83aa07e57
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-01-09 12:07:08 +0900

    refactor: improve solution with Arrays.equals() (#120883)

--- src/programmers/level0/array/login_validation/Solution_Arrays_equals.java
+++ src/programmers/level0/array/login_validation/Solution_Arrays_equals.java
@@ -1,17 +1,16 @@
 package programmers.level0.array.login_validation;
 
-class Solution_element_compare {
-    public String solution(String[] id_pw, String[][] db) {
-        String answer = "";
+import java.util.Arrays;
 
-        for (String[] i : db) {
-            if (i == id_pw) { // 주소값 비교를 하게 되므로 잘못된 풀이
+class Solution_Arrays_equals {
+    public String solution(String[] id_pw, String[][] db) {
+        for (String[] dbInfo : db) {
+            if (Arrays.equals(dbInfo, id_pw)) {
                 return "login";
-            } else if (i[0].equals(id_pw[0])) {
+            } else if (dbInfo[0].equals(id_pw[0])) {
                 return "wrong pw";
             }
         }
-
         return "fail";
     }
 }
