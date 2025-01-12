35b1be5621e68e6ecb273dc1340fe6a1b27fb928
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-01-12 16:44:45 +0900

    solve: [BOJ/Silver IV] Stack (#10828)

---
+++ src/baekjoon/silver/stack_10828/Main.java
@@ -0,0 +1,51 @@
+package baekjoon.silver.stack_10828;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> results = new ArrayList<>();
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            switch (command) {
+                case "push":
+                    int X = Integer.parseInt(st.nextToken());
+                    list.add(X);
+                    break;
+                case "pop":
+                    if (list.isEmpty()) {
+                        results.add(-1);
+                    } else {
+                        results.add(list.remove(list.size() - 1));
+                    }
+                    break;
+                case "size":
+                    results.add(list.size());
+                    break;
+                case "empty":
+                    if (list.isEmpty()) {
+                        results.add(1);
+                    } else {
+                        results.add(0);
+                    } break;
+                case "top":
+                    if (list.isEmpty()) {
+                        results.add(-1);
+                    } else {
+                        results.add(list.get(list.size() - 1));
+                    } break;
+            }
+        }
+        for (Integer result : results) {
+            System.out.println(result);
+        }
+    }
+}
