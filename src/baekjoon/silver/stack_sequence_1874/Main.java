c5a2d727fd69414e3d0c74b0ecae787dafac65ea
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-01-14 23:23:14 +0900

    refactor: improve solution with O(n) approach (#1874)

--- src/baekjoon/silver/stack_sequence_1874/Main.java
+++ src/baekjoon/silver/stack_sequence_1874/Main.java
@@ -3,39 +3,34 @@ package baekjoon.silver.stack_sequence_1874;
 import java.io.*;
 import java.util.*;
 
-public class Main_Timeout {
-    public static void main(String[] args) throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> history = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        boolean isValid = true;
         Stack<Integer> stack = new Stack<>();
+        int currentNum = 1; // 다음에 push 할 수
 
-        for (int i = 0; i < n; i++) {
-            int k = Integer.parseInt(br.readLine());
-            while (true) {
-                if (stack.isEmpty()) {
-                    stack.push(1);
-                    sb.append("+").append('\n');
-                } else if (stack.peek() < k) {
-                    for (int y = stack.peek() + 1; y <= k; y++) {
-                        if (!history.contains(y)) { // O(n) 소요
-                            stack.push(y);
-                            sb.append("+").append('\n');
-                        }
-                    }
-                } else if (!stack.isEmpty() && stack.peek() == k) {
-                    history.add(stack.pop());
-                    sb.append("-").append('\n');
-                    break;
-                } else {
-                    isValid = false;
-                    break;
-                }
+        for(int i = 0; i < n; i++) {
+            int target = Integer.parseInt(br.readLine());
+
+            // target까지 push
+            while(currentNum <= target) {
+                stack.push(currentNum++);
+                sb.append("+\n");
+            }
+
+            // pop할 수 있는지 확인
+            if(!stack.isEmpty() && stack.peek() == target) {
+                stack.pop();
+                sb.append("-\n");
+            } else {
+                System.out.println("NO");
+                return;
             }
         }
-        System.out.println(isValid ? sb : "NO");
+
+        System.out.print(sb);
     }
 }
