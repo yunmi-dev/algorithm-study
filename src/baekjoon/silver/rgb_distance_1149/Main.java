75b50780fe13db11214d77b448f0f831c8439d1e
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-01-20 23:40:34 +0900

    solve: [BOJ/Silver I] RGB distance - DP (#1149) [WIP]

---
+++ src/baekjoon/silver/rgb_distance_1149/Main.java
@@ -0,0 +1,27 @@
+package baekjoon.silver.rgb_distance_1149;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] costs = new int[N][3];
+        int minCostSum = 1000 * N;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            costs[i][0] = Integer.parseInt(st.nextToken());
+            costs[i][1] = Integer.parseInt(st.nextToken());
+            costs[i][2] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(minCostSum);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
