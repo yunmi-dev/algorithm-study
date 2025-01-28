ab597b9eb391ff420f14e82d18040c643f419614
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-01-28 23:09:54 +0900

    solve: [BOJ/Bronze II] Fibonacci number 5 (#10870)

---
+++ src/baekjoon/bronze/fibonacci_number_5/Main.java
@@ -0,0 +1,23 @@
+package baekjoon.bronze.fibonacci_number_5;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] fibo = new int[21];
+        fibo[0] = 0;
+        fibo[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            fibo[i] = fibo[i-2] + fibo[i-1];
+        }
+        bw.write(String.valueOf(fibo[n]));
+        bw.flush();
+        bw.close();
+    }
+}
