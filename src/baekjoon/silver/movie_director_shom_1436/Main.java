11a16babd1aff2bc4bdc20bb52eb325399b17ecc
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-02-01 23:07:17 +0900

    solve: [BOJ/Silver V] Movie director Shom (#1436)

--- src/baekjoon/silver/movie_director_shom_1436/Main.java
+++ src/baekjoon/silver/movie_director_shom_1436/Main.java
@@ -1,36 +1,24 @@
-package baekjoon.bronze.gcd_and_lcm_2609;
+package baekjoon.silver.movie_director_shom_1436;
 
 import java.io.*;
-import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int smallNum = Math.min(a, b);
-        int bigNum = Math.max(a, b);
+        int N = Integer.parseInt(br.readLine());
+        int sequence = 0;
+        int intCurrent = 665;
 
-        // 최대공약수
-        for (int gcd = smallNum; gcd > 0; gcd--) {
-            if (smallNum % gcd == 0 && bigNum % gcd == 0) {
-                System.out.println(gcd);
-                break;
+        while (sequence != N) {
+            intCurrent++;
+            String strCurrent = Integer.toString(intCurrent);
+            if (strCurrent.contains("666")) {
+                sequence++;
             }
         }
-
-        // 최소공배수
-        int i = 1;
-        while (true) {
-            int lcm = smallNum * i++;
-            if (lcm % bigNum == 0) {
-                System.out.println(lcm);
-                break;
-            }
-        }
+        System.out.println(intCurrent);
     }
 }
