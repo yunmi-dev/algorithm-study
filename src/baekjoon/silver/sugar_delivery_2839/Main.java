d4559d0e6bf0d76ad9ffc6b56891be359cd75eaa
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-02-01 21:52:52 +0900

    solve: [BOJ/Silver IV] Sugar delivery (#2839)

--- src/baekjoon/silver/sugar_delivery_2839/Main.java
+++ src/baekjoon/silver/sugar_delivery_2839/Main.java
@@ -1,6 +1,7 @@
-package baekjoon.bronze.sugar_delivery_2839;
+package baekjoon.silver.sugar_delivery_2839;
 
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
@@ -14,6 +15,20 @@ public class Main {
         int count5 = N / 5 + 1;
 
         int[][] numbers = new int[count3][count5];
+        ArrayList<Integer> exists = new ArrayList<>();
+        for (int row = 0; row < count3; row++) {
+            for (int col = 0; col < count5; col++) {
+                numbers[row][col] = 3 * row + 5 * col;
+                if (numbers[row][col] == N) {
+                    exists.add(row+col);
+                }
+            }
+        }
+        if (exists.isEmpty()) {
+            System.out.println(-1);
+        } else {
+            System.out.println(Collections.min(exists));
+        }
 
     }
 }
