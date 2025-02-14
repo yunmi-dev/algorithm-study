4591b0421bbaee6cb8e9e906a8b299cc7908c882
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-02-14 10:53:51 +0900

    solve: [BOJ/Silver II] LIS - DP (#11053) [WIP]

--- src/baekjoon/silver/LIS_11053/Main.java
+++ src/baekjoon/silver/LIS_11053/Main.java
@@ -1,18 +1,32 @@
 package baekjoon.silver.LIS_11053;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        ArrayList<Integer> counts = new ArrayList();
-        int result = 0;
+
+        int[] sequence = new int[N + 1];
+        int[] dp = new int[N + 1]; // 해당 번째까지 가장 긴 수열의 길이
+
+        for (int i = 1; i <= N; i++) {
+            sequence[i] = Integer.parseInt(st.nextToken());
+        }
+
+        ArrayList<Integer> firstList = new ArrayList<>();
+
+        dp[1] = 1;
+        firstList.add(1);
+
+        for (int i = 2; i <= N; i++) {
+            if (sequence[i] > sequence[i - 1]) {
+
+            }
+        }
 
     }
 }
