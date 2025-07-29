c924286acdc7128b71ea5ffd603024aa6849d27c
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-07-29 19:06:07 +0900

    solve: [BOJ/Gold I] Marble Escape 2 (#13460) - WIP

---
+++ src/baekjoon/silver/DFS_and_BFS/Main.java
@@ -0,0 +1,34 @@
+package baekjoon.silver.ATM_11399;
+
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            String PS = br.readLine();
+
+            Stack<Character> stack = new Stack<>();
+            for (char ch : PS.toCharArray()) {
+                if (!stack.isEmpty() && stack.peek() == '(' && ch == ')') {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            }
+            if (!stack.isEmpty()) {
+                sb.append("NO").append('\n');
+            } else {
+                sb.append("YES").append('\n');
+            }
+        }
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+    }
+}
